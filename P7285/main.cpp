b782057274d6cc07d099d020fc33bd96063d4898
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-29 09:27:22 +0800

    add p7285

---
+++ P7285/main.cpp
@@ -0,0 +1,26 @@
+// luogu p7285
+
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int n{};
+    cin >> n;
+    int *a{new int[n]};
+    for (int i{}; i < n; ++i) {
+        cin >> a[i];
+    }
+    // no idea
+    delete[] a;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int t{};
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+    return 0;
+}
