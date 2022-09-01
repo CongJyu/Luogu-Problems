53e836e5e3744e25b58505abe9ab551a65e98b5b
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-09-01 15:10:49 +0800

    add p1734

--- P1734/main.cpp
+++ P1734/main.cpp
@@ -4,10 +4,22 @@
 
 using namespace std;
 
+int a[1001]{}, dp[1001]{};
+
 int main() {
     ios::sync_with_stdio(false);
     int s{};
     cin >> s;
-    // no idea
+    for (int i{1}; i <= s; ++i) {
+        for (int j{i * 2}; j <= s; j += i) {
+            a[j] += i;
+        }
+    }
+    for (int i{1}; i <= s; ++i) {
+        for (int j{i}; j <= s; ++j) {
+            dp[j] = max(dp[j], dp[j - i] + a[i]);
+        }
+    }
+    cout << dp[s] << endl;
     return 0;
 }
