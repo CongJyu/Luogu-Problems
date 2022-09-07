bbd1d9c2eade3a57f4ddec8b3cd2e19e626c9ee1
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-09-07 23:38:10 +0800

    add p1317

---
+++ P1317/main.cpp
@@ -0,0 +1,19 @@
+// luogu p1317
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{};
+    cin >> n;
+    int *a{new int[n]};
+    for (int i{}; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i{1}; i < n - 1; ++i) {
+        
+    }
+    return 0;
+}
