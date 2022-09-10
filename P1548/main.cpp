63e7a54cb13baea6b9ce8c5a03a6c6081d8db534
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-09-10 23:24:00 +0800

    add p1548

---
+++ P1548/main.cpp
@@ -0,0 +1,15 @@
+// luogu p1548
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{}, m{};
+    cin >> n >> m;
+    int square{}, rectangle{};
+    square = ((m + 1) * (n - 1) * m * n) / 4;
+    for ()
+    return 0;
+}
