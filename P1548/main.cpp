4ff43bbf3afd5c8d5cae78eb58dd4e49e34d9d24
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-09-10 23:27:00 +0800

    add p1548

--- P1548/main.cpp
+++ P1548/main.cpp
@@ -8,8 +8,10 @@ int main() {
     ios::sync_with_stdio(false);
     int n{}, m{};
     cin >> n >> m;
-    int square{}, rectangle{};
-    square = ((m + 1) * (n - 1) * m * n) / 4;
-    for ()
+    int square{}, rectangle{((m + 1) * (n + 1) * m * n) / 4};
+    for (; m >= 1 and n >= 1; m--, n--) {
+        square += m * n;
+    }
+    cout << square << " " << rectangle - square << endl;
     return 0;
 }
