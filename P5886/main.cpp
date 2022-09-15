d5fd4bcea2b9bf6de4e5e0e53d7407f06dd3cdc1
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-09-16 01:01:06 +0800

    add p5886

---
+++ P5886/main.cpp
@@ -0,0 +1,13 @@
+// luogu p5886
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{}, m{}, p{};
+    cin >> n >> m >> p;
+    
+    return 0;
+}
