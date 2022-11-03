ddcb851046d66d4363e23a537d7727fce4fc31ab
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-04 00:58:25 +0800

    add b2004

---
+++ B2004/main.cpp
@@ -0,0 +1,14 @@
+// luogu b2004
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int a{}, b{}, c{};
+    cin >> a >> b >> c;
+    cout << setw(8) << a << " " << setw(8) << b << " " << setw(8) << c << endl;
+    return 0;
+}
