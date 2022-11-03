02c28c165165fa4135833e7e0708ee299807cc5f
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-04 00:47:51 +0800

    add b2001

---
+++ B2001/main.cpp
@@ -0,0 +1,14 @@
+// luogu b2001
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    long long a{}, b{};
+    cin >> a >> b;
+    long long result{a + b};
+    cout << result << endl;
+    return 0;
+}
