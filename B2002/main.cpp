d7e62f00c2e30ae76cffd087139c375c18a2caa3
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-04 00:48:59 +0800

    add b2002

--- B2002/main.cpp
+++ B2002/main.cpp
@@ -1,6 +1,11 @@
+// luogu b2002
+
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios::sync_with_stdio(false);
+    cout << "Hello,World!" << endl;
     return 0;
 }
