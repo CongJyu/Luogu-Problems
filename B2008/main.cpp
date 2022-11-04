7b7bad21e0543211b26de779205187ea60a36f13
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-05 00:50:11 +0800

    add b2008

---
+++ B2008/main.cpp
@@ -0,0 +1,13 @@
+// luogu b2008
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int a{}, b{}, c{};
+    cin >> a >> b >> c;
+    cout << (a + b) * c << endl;
+    return 0;
+}
