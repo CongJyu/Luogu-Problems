18f0114ac701400f209bd1ce8ab3c31b874a06c9
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-05 00:36:46 +0800

    add b2005

---
+++ B2005/main.cpp
@@ -0,0 +1,15 @@
+// luogu b2005
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    char input{};
+    cin >> input;
+    cout << "  " << input << endl;
+    cout << " " << input << input << input << endl;
+    cout << input << input << input << input << input << endl;
+    return 0;
+}
