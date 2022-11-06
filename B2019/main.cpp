ce9195b6c6beb6a91c2ce425b3c82efa908ab0c7
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-06 20:21:08 +0800

    add b2019

---
+++ B2019/main.cpp
@@ -0,0 +1,13 @@
+// luogu b2019
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int input{};
+    cin >> input;
+    cout << (bool) input << endl;
+    return 0;
+}
