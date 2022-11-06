5e11a7795de732ac56b1fcafd2b1363ce9c8b42a
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-06 20:16:48 +0800

    add b2017

---
+++ B2017/main.cpp
@@ -0,0 +1,13 @@
+// luogu b2017
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    char input{};
+    cin >> input;
+    cout << (int) input << endl;
+    return 0;
+}
