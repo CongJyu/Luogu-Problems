3761edffb768fb9fe60a47c7f815a1fe74b25d6d
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-06 20:14:14 +0800

    add b2016

---
+++ B2016/main.cpp
@@ -0,0 +1,13 @@
+// luogu b2016
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    double a{};
+    cin >> a;
+    cout << (long long) a << endl;
+    return 0;
+}
