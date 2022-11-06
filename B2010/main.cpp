1ccc71f6bc1124e18738a8f929fe45ce4f27900b
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-11-06 18:31:17 +0800

    add b2010

---
+++ B2010/main.cpp
@@ -0,0 +1,13 @@
+// luogu b2010
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int a{}, b{};
+    cin >> a >> b;
+    cout << a / b << " " << a % b << endl;
+    return 0;
+}
