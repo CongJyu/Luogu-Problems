6eef77bbd59552c4190f4730710c184e6fcaf781
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-17 00:33:58 +0800

    Add b2021

---
+++ B2021/main.cpp
@@ -0,0 +1,14 @@
+// luogu b2021
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    float input{};
+    cin >> input;
+    cout << fixed << setprecision(3) << input << endl;
+    return 0;
+}
