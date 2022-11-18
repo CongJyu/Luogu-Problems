f5f963acf0761cdc4a8d060c38813513057c06ce
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-18 23:47:40 +0800

    add b2025

---
+++ B2025/main.cpp
@@ -0,0 +1,15 @@
+// luogu b2025
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout << "  *\n"
+            " ***\n"
+            "*****\n"
+            " ***\n"
+            "  *" << endl;
+    return 0;
+}
