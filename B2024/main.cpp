dbfdc42e28b2181c07a0c953e0e0a51433dbf490
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-18 23:45:37 +0800

    add b2024

---
+++ B2024/main.cpp
@@ -0,0 +1,16 @@
+// luogu b2024
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    double input{};
+    cin >> input;
+    printf("%f\n", input);
+    printf("%.5f\n", input);
+    printf("%e\n", input);
+    printf("%g\n", input);
+    return 0;
+}
