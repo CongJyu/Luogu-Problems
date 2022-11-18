6d7a4e23cf756029c717758ab1e83abcfb2fb0b8
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-18 23:34:17 +0800

    Add b2022

---
+++ B2022/main.cpp
@@ -0,0 +1,14 @@
+// luogu b2022
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    double input{};
+    cin >> input;
+    cout << fixed << setprecision(12) << input << endl;
+    return 0;
+}
