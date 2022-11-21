a775c1de6efa8e0b1d3e81674383439657d96bbd
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-22 00:41:16 +0800

    add b2028

---
+++ B2028/main.cpp
@@ -0,0 +1,13 @@
+// luogu b2028
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    char a[3]{};
+    cin >> a[0] >> a[1] >> a[2];
+    cout << a[2] << a[1] << a[0] << endl;
+    return 0;
+}
