955a4f00af683cddb3484a4cc14aab23b47beec5
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-27 02:05:42 +0800

    add b2033

---
+++ B2033/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/rainchen/CodingArea/Luogu-Problems/B2033
+# Build directory: /Users/rainchen/CodingArea/Luogu-Problems/B2033/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ B2033/main.cpp
@@ -0,0 +1,14 @@
+// luogu b2033
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    unsigned long long a{}, b{};
+    cin >> a >> b;
+    unsigned long long result{a * b};
+    cout << result << endl;
+    return 0;
+}
