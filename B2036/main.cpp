f88756ddc763d183c25495bf849958b3e6857bd5
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-11-30 01:52:39 +0800

    accepted

--- B2036/main.cpp
+++ B2036/main.cpp
@@ -1,21 +1,16 @@
 // luogu b2036
 
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    string s{};
-    cin >> s;
-    if (s[0] == '-') {
-        for (int i{1}; i < (int) s.length(); ++i) {
-            cout << s[i];
-        }
-        cout << endl;
-    } else {
-        cout << s << endl;
-    }
+    double input{};
+    cin >> input;
+    double output{abs(input)};
+    cout << fixed << setprecision(2) << output << endl;
     return 0;
 }
