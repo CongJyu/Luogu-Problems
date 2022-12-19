18af0ce8bd407caa092699cd3eeffe4cfa42b1d5
CongJyu <87171700+CongJyu@users.noreply.github.com>
2022-12-19 23:00:43 +0800

    add b2072

---
+++ B2072/main.cpp
@@ -0,0 +1,14 @@
+// luogu b2072
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{};
+    cin >> n;
+    int result{(1 + n) * n / 2};
+    cout << result << endl;
+    return 0;
+}
